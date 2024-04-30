0218e22d58ff77259308c2b3115ae5a527b6e821
mailto2020wa86196@wilp.bits-pilani.ac.in <kuruvasaikiran11@gmail.com>
2024-04-30 19:58:02 +0530

    adding some function from main branch

--- file_1.c
+++ file_1.c
@@ -1,6 +1,8 @@
 int main(){
-     int n1, n2;
+     int n1, n2, sum;
      printf("Enter 2 numbers");
      scanf("%d %d");
-     prinff("%d %d", n1, n2);
+     sum = n1 + n2;
+     prinf("%d %d", n1, n2);
+     printf("%d ", sum);
 }
